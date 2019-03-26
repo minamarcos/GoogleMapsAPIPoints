5e643fc553032366c10e643f4196a74a06edce31
mina marcos <minamarcos@gmail.com>
2019-03-26 13:38:09 -0700

    adding local icons

--- GoogleMapsAPIMarkPopup/js/places.js
+++ GoogleMapsAPIMarkPopup/js/places.js
@@ -1,126 +1,137 @@
-var places = [{
-        hours: "9am-5pm",
-        bio: "The monasteries of St. Anthony and St. Paul are the oldest Christian monasteries; they both date back to the 4th century AD, when monks began to settle at the foot of the Red Sea mountain of Galala Al-Qibliya, in hidden cliffs and caves. Both monasteries are considered as the holiest sites of the Coptic Church in Egypt. Located off the Zafarana Road leading to Beni Suef, some 70 km south from Ain Sukhna, 45 km from Hurghada City, the Monastery of St Anthony is a large complex encircled by high walls, including churches, chapels, a bakery, a garden ...",
-        type: "Coptic-Egypt",
-        url: "google.com",
-        img: "STanthony.jpg",
-        wikiLink: "STanthony.com",
-        coords: {
-            lat: 28.556259,
-            lng: 33.974819
-        },
-        iconImage: "https://cdn.iconscout.com/icon/premium/png-256-thumb/cross-397-883862.png",
-        name: "St Anthony Monastery"
-    }, {
-        hours: "9am-5pm",
-        bio: "Prince Mohamed Ali Palace Manial or Manial Palace is an architectural masterpiece of great value, located Manial a neighborhood located on the island of Roda Nile in Cairo. Surrounded by gardens singing, and still retains its affected architecture Maghreb style, despite the passage of time. And is used today as a museum Belonged to Prince Mohammed Ali, who for crisis Unfortunately to assume the reins of government property on the throne of probation, he was crown on the throne of Egypt before the birth of the former King Farouk and lived for so anticipating dream not fo",
-        type: "old-cairo",
-        url: "google.com",
-        img: "oldCairo.jpg",
-        wikiLink: "oldCairo.com",
-        coords: {
-            lat: 30.105860,
-            lng: 31.204960
-        },
-        iconImage: "../img/star.png",
-        name: "Mohamed Ali Tawfeq palace"
+var places = [
+  {
+    hours: "9am-5pm",
+    bio:
+      "The monasteries of St. Anthony and St. Paul are the oldest Christian monasteries; they both date back to the 4th century AD, when monks began to settle at the foot of the Red Sea mountain of Galala Al-Qibliya, in hidden cliffs and caves. Both monasteries are considered as the holiest sites of the Coptic Church in Egypt. Located off the Zafarana Road leading to Beni Suef, some 70 km south from Ain Sukhna, 45 km from Hurghada City, the Monastery of St Anthony is a large complex encircled by high walls, including churches, chapels, a bakery, a garden ...",
+    type: "Coptic-Egypt",
+    url: "google.com",
+    img: "STanthony.jpg",
+    wikiLink: "STanthony.com",
+    coords: {
+      lat: 28.556259,
+      lng: 33.974819
     },
-    {
-        hours: "9am-5pm",
-        bio: "Located approximately 50 km southwest of Alexandria, the small village-town of Abu-Mina is home to St. Mena (Mina) monastery. St Mena is believed to have fallen as a martyr in the early 4th century when the Roman Empire was persecuting Christians. A modern monastery has been built on the location of an ancient church, where the Saint remains are believed to be buried. A German archeological team has been working at Abu Mina since 1969. Buses depart regularly from Alexandria new station (Baheej Station), to Abu-Mina. After reaching Abu-Mina, yo",
-        type: "Coptic-Egypt",
-        url: "google.com",
-        img: "stmina.jpg",
-        wikiLink: "stmina.com",
-        coords: {
-            lat: 29.81785,
-            lng: 30.94858
-        },
-        iconImage: "https://cdn.iconscout.com/icon/premium/png-256-thumb/cross-397-883862.png",
-        name: "St Mena Monastery"
+    iconImage: "../img/cross.png",
+    name: "St Anthony Monastery"
+  },
+  {
+    hours: "9am-5pm",
+    bio:
+      "Prince Mohamed Ali Palace Manial or Manial Palace is an architectural masterpiece of great value, located Manial a neighborhood located on the island of Roda Nile in Cairo. Surrounded by gardens singing, and still retains its affected architecture Maghreb style, despite the passage of time. And is used today as a museum Belonged to Prince Mohammed Ali, who for crisis Unfortunately to assume the reins of government property on the throne of probation, he was crown on the throne of Egypt before the birth of the former King Farouk and lived for so anticipating dream not fo",
+    type: "old-cairo",
+    url: "google.com",
+    img: "oldCairo.jpg",
+    wikiLink: "oldCairo.com",
+    coords: {
+      lat: 30.10586,
+      lng: 31.20496
     },
-    {
-        hours: "9am-5pm",
-        bio: "Located at the heart of Islamic Cairo, the Al-Azhar complex, mosque and university, does not only house the oldest university in the world but it is also the place where the graduation black gowns originated from. The costume worn by students all around the world during their graduation seems to have been inspired by the flowing robes of the Islamic Scholars graduating from Al-Azhar. The University is now distributed between different buildings, but the mosque, founded by Jawhar al-Siqilly, the Fatimid conqueror of Egypt, in 970 is still a true ",
-        type: "Islamic-Egypt",
-        url: "google.com",
-        img: "Al-Azhar.jpg",
-        wikiLink: "Azhar.com",
-        coords: {
-            lat: 30.04636,
-            lng: 31.26423
-        },
-        iconImage: "../img/mosque.png",
-        name: "Al-Azhar Mosque"
+    iconImage: "../img/star.png",
+    name: "Mohamed Ali Tawfeq palace"
+  },
+  {
+    hours: "9am-5pm",
+    bio:
+      "Located approximately 50 km southwest of Alexandria, the small village-town of Abu-Mina is home to St. Mena (Mina) monastery. St Mena is believed to have fallen as a martyr in the early 4th century when the Roman Empire was persecuting Christians. A modern monastery has been built on the location of an ancient church, where the Saint remains are believed to be buried. A German archeological team has been working at Abu Mina since 1969. Buses depart regularly from Alexandria new station (Baheej Station), to Abu-Mina. After reaching Abu-Mina, yo",
+    type: "Coptic-Egypt",
+    url: "google.com",
+    img: "stmina.jpg",
+    wikiLink: "stmina.com",
+    coords: {
+      lat: 29.81785,
+      lng: 30.94858
     },
-    {
-        hours: "9am-5pm",
-        bio: "The Karnak Temple Complex, commonly known as Karnak, comprises a vast mix of decayed temples, chapels, pylons, and other buildings near Luxor, in Egypt.......",
-        type: "Ancient-Egypt",
-        url: "google.com",
-        img: "Karnak.jpg",
-        wikiLink: "googl.com",
-        coords: {
-            lat: 26.06996,
-            lng: 32.08605
-        },
-        iconImage: "../img/pherph.png",
-        name: "karnak temple"
+    iconImage: "../img/cross.png",
+    name: "St Mena Monastery"
+  },
+  {
+    hours: "9am-5pm",
+    bio:
+      "Located at the heart of Islamic Cairo, the Al-Azhar complex, mosque and university, does not only house the oldest university in the world but it is also the place where the graduation black gowns originated from. The costume worn by students all around the world during their graduation seems to have been inspired by the flowing robes of the Islamic Scholars graduating from Al-Azhar. The University is now distributed between different buildings, but the mosque, founded by Jawhar al-Siqilly, the Fatimid conqueror of Egypt, in 970 is still a true ",
+    type: "Islamic-Egypt",
+    url: "google.com",
+    img: "Al-Azhar.jpg",
+    wikiLink: "Azhar.com",
+    coords: {
+      lat: 30.04636,
+      lng: 31.26423
     },
-    {
-        hours: "9am-5pm",
-        bio: "Al-Rifa'i Mosque, is located in Cairo, Egypt, in Midan al-Qal'a, adjacent to the Cairo Citadel. The building is located opposite the Mosque-Madrassa of Sultan Hassan, which dates from around 1361, and was architecturally conceived as a complement to the older structure",
-        type: "Islamic-Egypt",
-        url: "xxxxxx",
-        img: "../img/ElRefaiMosqueinCairo.jpg",
-        wikiLink: "https://en.wikipedia.org/wiki/Al-Rifa'i_Mosque",
-        coords: {
-            lat: 30.155030,
-            lng: 31.311630
-        },
-        iconImage: "../img/mosque.png",
-        name: "Al-Rifa'i Mosque"
+    iconImage: "../img/mosque.png",
+    name: "Al-Azhar Mosque"
+  },
+  {
+    hours: "9am-5pm",
+    bio:
+      "The Karnak Temple Complex, commonly known as Karnak, comprises a vast mix of decayed temples, chapels, pylons, and other buildings near Luxor, in Egypt.......",
+    type: "Ancient-Egypt",
+    url: "google.com",
+    img: "Karnak.jpg",
+    wikiLink: "googl.com",
+    coords: {
+      lat: 26.06996,
+      lng: 32.08605
     },
-    {
-        hours: "9am-5pm",
-        bio: "Downtown Cairo was designed by prestigious French architects who were commissioned by Khedive Ismail during his visit to Paris, and since then he wanted to make the Egyptian Kingdom capital better than Paris and to be the jewel of the orient. It was he who stressed the importance of European style urban planning in Cairo, to include broad, linear gridded streets, geometric harmony and modern European architectural style. It was once home to the prosperous elite of late 19th and early 20th century Cairo. It is a relic of a bygone era — Egypt's Belle epoque — and demonstrates the vision for developing Egypt. Yet decades of neglect by the neighbourhood's landlords and tenants following the burning of most Cairo's buildings during the popular Cairo fire incident prior the 1952 Revolution led by Gamal Abdel Nasser, and the ensuing departure of the upper classes, have left ",
-        type: "old-cairo",
-        url: "https://en.wikipedia.org/wiki/Downtown_Cairo",
-        img: "../img/amadElDeenSt.jpg",
-        wikiLink: "https://en.wikipedia.org/wiki/Downtown_Cairo",
-        coords: {
-            lat: 30.048369,
-            lng: 31.246815
-        },
-        iconImage: "../img/star.png",
-        name: "Emad El-Deen St,Cairo Down Town"
+    iconImage: "../img/pherph.png",
+    name: "karnak temple"
+  },
+  {
+    hours: "9am-5pm",
+    bio:
+      "Al-Rifa'i Mosque, is located in Cairo, Egypt, in Midan al-Qal'a, adjacent to the Cairo Citadel. The building is located opposite the Mosque-Madrassa of Sultan Hassan, which dates from around 1361, and was architecturally conceived as a complement to the older structure",
+    type: "Islamic-Egypt",
+    url: "xxxxxx",
+    img: "../img/ElRefaiMosqueinCairo.jpg",
+    wikiLink: "https://en.wikipedia.org/wiki/Al-Rifa'i_Mosque",
+    coords: {
+      lat: 30.15503,
+      lng: 31.31163
     },
-    {
-        hours: "9am-5pm",
-        bio: "The Qasr El Nil Bridge, also commonly spelled Kasr El Nil Bridge, is a historic structure dating from 1931 and replaced the first bridge to span the Nile River in central Cairo, Egypt. It connects Tahrir Square in downtown Cairo to the moder",
-        type: "old-cairo",
-        url: "xxxxxx",
-        img: "../img/QasrElNeelBridge.jpg",
-        wikiLink: "xxxxxxxxxxxxxx",
-        coords: {
-            lat: 30.043966,
-            lng: 31.231620
-        },
-        iconImage: "../img/star.png",
-        name: "Qasr El Nil Bridge"
+    iconImage: "../img/mosque.png",
+    name: "Al-Rifa'i Mosque"
+  },
+  {
+    hours: "9am-5pm",
+    bio:
+      "Downtown Cairo was designed by prestigious French architects who were commissioned by Khedive Ismail during his visit to Paris, and since then he wanted to make the Egyptian Kingdom capital better than Paris and to be the jewel of the orient. It was he who stressed the importance of European style urban planning in Cairo, to include broad, linear gridded streets, geometric harmony and modern European architectural style. It was once home to the prosperous elite of late 19th and early 20th century Cairo. It is a relic of a bygone era — Egypt's Belle epoque — and demonstrates the vision for developing Egypt. Yet decades of neglect by the neighbourhood's landlords and tenants following the burning of most Cairo's buildings during the popular Cairo fire incident prior the 1952 Revolution led by Gamal Abdel Nasser, and the ensuing departure of the upper classes, have left ",
+    type: "old-cairo",
+    url: "https://en.wikipedia.org/wiki/Downtown_Cairo",
+    img: "../img/amadElDeenSt.jpg",
+    wikiLink: "https://en.wikipedia.org/wiki/Downtown_Cairo",
+    coords: {
+      lat: 30.048369,
+      lng: 31.246815
     },
-    {
-        hours: "9am-5pm",
-        bio: "The Monastery of Saint Pishoy (also spelled Bishoy, Pshoi, or Bishoi[1]) in Wadi El Natrun, Beheira Governorate, Egypt, is the most famous monastery of the Coptic Orthodox Church of Alexandria named after Pishoy. It is the easternmost of the four current monasteries of Wadi el Natrun",
-        type: "Coptic-Egypt",
-        url: "xxxxxx",
-        img: "../img/stbeshoy.jpg",
-        wikiLink: "https://en.wikipedia.org/wiki/Monastery_of_Saint_Pishoy",
-        coords: {
-            lat: '222222222',
-            lng: '333333333'
-        },
-        iconImage: "../img/pherph.png",
-        name: "Monastery of Saint Pishoy"
-    }
+    iconImage: "../img/star.png",
+    name: "Emad El-Deen St,Cairo Down Town"
+  },
+  {
+    hours: "9am-5pm",
+    bio:
+      "The Qasr El Nil Bridge, also commonly spelled Kasr El Nil Bridge, is a historic structure dating from 1931 and replaced the first bridge to span the Nile River in central Cairo, Egypt. It connects Tahrir Square in downtown Cairo to the moder",
+    type: "old-cairo",
+    url: "xxxxxx",
+    img: "../img/QasrElNeelBridge.jpg",
+    wikiLink: "xxxxxxxxxxxxxx",
+    coords: {
+      lat: 30.043966,
+      lng: 31.23162
+    },
+    iconImage: "../img/star.png",
+    name: "Qasr El Nil Bridge"
+  },
+  {
+    hours: "9am-5pm",
+    bio:
+      "The Monastery of Saint Pishoy (also spelled Bishoy, Pshoi, or Bishoi[1]) in Wadi El Natrun, Beheira Governorate, Egypt, is the most famous monastery of the Coptic Orthodox Church of Alexandria named after Pishoy. It is the easternmost of the four current monasteries of Wadi el Natrun",
+    type: "Coptic-Egypt",
+    url: "xxxxxx",
+    img: "../img/stbeshoy.jpg",
+    wikiLink: "https://en.wikipedia.org/wiki/Monastery_of_Saint_Pishoy",
+    coords: {
+      lat: 30.359598,
+      lng: 30.319314
+    },
+    iconImage: "../img/cross.png",
+    name: "Monastery of Saint Pishoy"
+  }
 ];
